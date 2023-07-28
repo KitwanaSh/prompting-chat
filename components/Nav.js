548d6dbd1ca4585a9f7d4a661088a020001d095d
KitwanaSh <rodriguekitweze@gmail.com>
2023-07-28 19:32:58 +0200

    Feat: Start with providers

--- components/Nav.js
+++ components/Nav.js
@@ -7,7 +7,15 @@ import {signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 function Nav() {
   const isUserLoggedIn = true;
+  const [provider, setProvider] = useState(null)
 
+  useEffect(() => {
+    const setProvider = async () => {
+      const response = await getProviders();
+
+      setProvider(response)
+    }
+  }, [])
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href="/" className='flex gap-2 flex-center'>
@@ -27,6 +35,15 @@ function Nav() {
           <div className='flex gap-3 md:gap-5'>
             <Link href="/create-prompt" className='black_btn'>Create Post</Link>
             <button type='button' className='outline_btn' onClick={signOut}>Sign Out</button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                alt='profile'
+                className='rounded-full'
+              />
+            </Link>
           </div>
         ) : (
           <>
