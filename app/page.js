d92a453a53d672d848c5add49e999451af362af5
KitwanaSh <rodriguekitweze@gmail.com>
2023-07-28 13:53:26 +0200

    Feat: Start the main page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompt</span>
+      </h1>
+    </section>
   )
 }
 
