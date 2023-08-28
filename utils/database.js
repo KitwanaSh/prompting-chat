a90c75a9cc18e95cf49d130834937bdeb3fb1a9d
KitwanaSh <rodriguekitweze@gmail.com>
2023-08-28 19:13:10 +0000

    Feat: the database

---
+++ utils/database.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false; // is connected
+
+export const connectDb = async () => {
+    mongoose.set('strictQuery', true);
+
+    if(isConnected) {
+        console.log('MongoDb is already connected!');
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGOBD_URI, {
+            dbName: "share_prompt",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        
+        isConnected = true;
+        console.log("MongoDb connected!")
+    } catch (error) {
+        console.log(error);
+    }
+}
