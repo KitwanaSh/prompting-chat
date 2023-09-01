eab1f13c2b1a24e99d51d3b6d8c77f75761107cc
KitwanaSh <rodriguekitweze@gmail.com>
2023-09-01 22:16:01 +0000

    Refractor: Concise the imports

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDb } from "@utils/database";
-
 import User from "@models/user"
 
 const handler = NextAuth({
